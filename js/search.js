7a2386f844f2df9784ad9be70242fae3bd449799
xinyu-git <925266768@qq.com>
2017-09-08 17:59:43 +0800

    hbw

---
+++ js/search.js
@@ -0,0 +1,98 @@
+﻿;(function($){
+		var defaultStr = '<a href="javascript:void(0);" val="-1">请选择</a>';
+		var proOld=[];//省数据-初始数据
+		var proNew=[];//市数据-去重数据
+		function oldArr(obj,oldArr,tdNum){
+			for(var i=0;i<obj.length;i++){
+				var proHtml=obj.eq(i).find(tdNum).html();
+				oldArr.push(proHtml);
+			}
+			return oldArr;
+		}
+
+		//去重
+		function findInArr(n, arr){
+		    for(var i=0; i<arr.length; i++){
+		        if(n==arr[i])return true;
+		    }
+		    return false;
+		}
+		function qcArr(arrOld,arrNew){
+			for(var i=0; i<arrOld.length; i++){
+	            if(findInArr(arrOld[i], arrNew)==false){
+	                arrNew.push(arrOld[i]);
+	            }
+	   		}
+	   		return arrNew;
+		}
+
+		oldArr($("#stores_tab tr"),proOld,'td:first')//所有省--数组
+		qcArr(proOld,proNew);//去重--省数组
+		showPro();
+		function showPro(){
+			var tempStr = defaultStr;
+			$.each(proNew,function(index,domEle){
+				tempStr += '<a href="javascript:void(0);" >' + domEle + '</a>';
+				$('div[name="province"]').html('<div class="opts mCustomScrollbar _mCS_1">'+ tempStr +'</div');
+				$('div[name="province"]').inputbox();
+			})
+		}
+
+		//市
+		showCity1();
+		//初始城市
+		function showCity1(){
+			var tempStr = defaultStr;
+			$('div[name="city"]').html('<div class="opts mCustomScrollbar _mCS_1">'+ tempStr +'</div');
+			$('div[name="city"]').inputbox();
+		}
+		function showCity(cityNew){
+			var tempStr = defaultStr;
+			$.each(cityNew,function(index,domEle){
+				tempStr += '<a href="javascript:void(0);" >' + domEle + '</a>';
+				$('div[name="city"]').html('<div class="opts mCustomScrollbar _mCS_1">'+ tempStr +'</div');
+				$('div[name="city"]').find('.opts').mCustomScrollbar();
+				console.log(tempStr)
+				$('<div class="selected">选择城市</div>').insertBefore($('div[name="city"]').children(':first'));
+			})
+		}
+
+		$('div[name="province"]').on('click','.opts a',function(){
+			var opts_html=$(this).html();
+			for(var i=0; i<proOld.length;i++){
+				if(proOld[i]!=opts_html){
+					if($(this).attr('val')==-1){
+						$('#stores_tab tr').removeClass('hide');
+						showCity1();
+						return;
+					}
+					$('#stores_tab tr').eq(i).addClass('hide')
+				}else{
+					$('#stores_tab tr').eq(i).removeClass('hide');
+				}
+			}
+			var cityOld=[];
+			var cityNew=[];
+			var showTr=$('#stores_tab tr').not('.hide');
+			oldArr(showTr,cityOld,'td:nth-child(2)')
+			qcArr(cityOld,cityNew);
+			showCity(cityNew);
+		})
+
+		$('div[name="city"]').on('click','.opts a',function(){
+			var opts_html=$(this).html();
+			var areaOld=[];
+			var showTr=$('#stores_tab tr')
+			oldArr(showTr,areaOld,'td:nth-child(2)')
+			for(var i=0;i<areaOld.length;i++){
+				if(areaOld[i]!=opts_html){
+					if($(this).attr('val')==-1){
+						return;
+					}
+					showTr.eq(i).addClass('hide');
+				}else{
+					showTr.eq(i).removeClass('hide')
+				}
+			}
+		})
+})(jQuery)
---
+++ js/selectbox.js
@@ -0,0 +1,67 @@
+﻿;(function($){
+	var opts = {};
+	var selectbox={
+		init:function(o){
+			var $o=$(o);
+			if($o.find('.opts > a.selected').html()){
+				_selectHtml=$o.find('.opts > a.selected').html()
+			}else{
+				if(bFlag){
+					_selectHtml='选择省份'
+				}else{
+					_selectHtml='选择城市'
+				}
+			}
+			//var _selectHtml=$o.find('.opts > a.selected').html()? $o.find('.opts > a.selected').html():$o.find('.opts > a:first').html();
+			$('<div class="selected">' + _selectHtml + '</div>').insertBefore($o.children(':first'));
+			$o.off('click').on('click', selectbox.toggle);
+			$o.off('click', '.opts  a').on('click', '.opts  a', selectbox.select);
+			$o.find('.opts').off('mouseenter mouseleave').on('mouseenter', selectbox.mouseenter).on('mouseleave', selectbox.mouseleave);
+			$(document).off('click', selectbox.hide).on('click', selectbox.hide);
+		},
+		toggle:function(e){
+			e.stopPropagation();
+			var $o = $(this);
+			var $opts = $o.children('.opts');
+			$o.find('a.selected').removeClass('none');
+			selectbox.hide(null, $('.sb').not($o));
+			$opts.toggle();
+		},
+		hide:function(e,objs){
+			var $o = objs ? objs : $('.sb');
+			$o.removeClass('sb_active').children('.opts').hide().find('a.selected').removeClass('none');
+		},
+		select:function(e){
+			e.stopPropagation();
+			var $o = $(this).parents('.sb:first');
+			$o.trigger('click');
+			$o.find('a.selected').removeClass('selected');
+			$(this).addClass('selected');
+			$o.find('div.selected').html($(this).html());
+		},
+		mouseenter:function(e){
+			e.stopPropagation();
+			var $o = $(this);
+			$o.find('a.selected').addClass('none');
+		},
+		mouseleave: function(e){
+			e.stopPropagation();
+			var $o = $(this);
+			$o.find('a.selected').removeClass('none');
+		},
+		_init:function(o){
+			var name=$(o).attr('name');
+			if(name=='province'){
+				bFlag=true;
+				selectbox.init(o);
+			}else if(name=='city'){
+				bFlag=false;
+				selectbox.init(o);
+			}
+		}
+	}
+	$.fn.inputbox=function(){
+		 //opts = $.extend({}, options);
+		selectbox._init(this)
+	}
+})(jQuery);
